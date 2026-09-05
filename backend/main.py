6f1369e194308b6f857ca29daa2b62f6c5df0ccd
kams <kevinmoradevwork@gmail.com>
2026-09-05 10:42:38 -0600

    "Test: Enviar correos a cuenta personal temporalmente"

--- backend/main.py
+++ backend/main.py
@@ -42,7 +42,7 @@ async def recibir_contacto(datos: FormularioContacto):
     # Preparar el paquete de datos para Resend
     params = {
         "from": "onboarding@resend.dev", # Resend usa este por defecto para pruebas
-        "to": ["administracion@industriasmorsol.com"],
+        "to": ["kevinmoradevwork@gmail.com"],
         "subject": f"🔴 NUEVA COTIZACIÓN: {datos.servicio} - {datos.nombre}",
         "reply_to": datos.correo, # ¡Mantenemos tu funcionalidad de responder directo al cliente!
         "html": f"""
